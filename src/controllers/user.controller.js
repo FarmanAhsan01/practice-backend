6910f0802abaeae79bd5a0f387f69eac85c6cb6a
FarmanAhsan01 <farmanahsan77@gmail.com>
2025-06-29 12:18:49 +0530

    add logout functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -73,6 +73,31 @@ const loginUser=asyncHandler(async(req,res)=>{
 
 })
 
+const logoutUser=asyncHandler(async(req,res)=>{
+    User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set:{
+          refrashToken:undefined
+        }
+      },
+      {
+        new:true
+      }
+    ) 
+     const options={
+         httpOnly:true,
+         secure:true
+     }
+
+     return res
+     .status(200)
+     .clearCookie("accessToken",options)
+     .clearCookie("refreshToken",options)
+     .json(new ApiResponse(200,{},"User Logged Out"))
+})
+
+
 
 
 const registerUser=asyncHandler(async(req,res)=>{
@@ -163,4 +188,11 @@ const registerUser=asyncHandler(async(req,res)=>{
       new ApiResponse(200,createdUser,"User registerd Successfully")
      )
 })
-export {registerUser}
+
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser
+
+}
