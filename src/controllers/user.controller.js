5ca91f985ec8736c3f25b28551635746db87058c
FarmanAhsan01 <farmanahsan77@gmail.com>
2025-07-04 11:10:31 +0530

    Add watchHistory and add all routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,7 @@ import { uploadOnCLoudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 import { response } from "express";
+import mongoose from "mongoose";
 
 const genrateAccessTokenAndRefreshToken=async(userId)=>{
      try {
@@ -407,6 +408,63 @@ const getUserChennalProfile=asyncHandler(async(req,res)=>{
   .json(new ApiResponse(200,channel[0],"User channel fetched successfully"))
 })
 
+const getWatchHistory=asyncHandler(async(req,res)=>{
+  const user=await user.aggregate([
+    {
+      $match:{
+        _id:new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from:"videos",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+
+              pipeline:[
+                {
+                  $project:{
+                    fullName:1,
+                    username:1,
+                    avatar:1
+                  }
+                },
+                {
+                  $addFields:{
+                    owner:{
+                      $first:"$owner"
+                    }
+                  }
+                }
+
+              ]
+            }
+
+          }
+        ]
+      }
+    }
+  ])
+  return res 
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,user[0].watchHistory,
+      "watch history fetched successfully"
+    )
+  )
+})
+
+
 export {
   registerUser,
   loginUser,
@@ -416,6 +474,8 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserChennalProfile,
+  getWatchHistory
 
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser ,refrehAccessToken} from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser ,refrehAccessToken, changeCurrentPassword, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChennalProfile, getWatchHistory} from "../controllers/user.controller.js";
 import { upload }from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/autho.middleware.js";
 
@@ -24,6 +24,12 @@ router.route("/login").post(loginUser)
 //secure router
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refrehAccessToken)
-
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,updateAccountDetails)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChennalProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 export default router;
